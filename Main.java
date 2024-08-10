ab44d6f6aec385da7a9657582b1ed2b7f11b6e03
Hideux123 <117778498+Hideux123@users.noreply.github.com>
2024-08-10 16:08:33 +0300

    Hello Word

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i < 11){
+            System.out.println("5 * " + i +" = " + i*5);
+            i++;
+        }
+    }
+}
